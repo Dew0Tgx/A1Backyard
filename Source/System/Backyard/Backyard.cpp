a57800beef2e53d61e49171e3e2d44592f314360
Aleksandr Knorre <a_knorre@wargaming.net>
2020-06-27 19:17:38 +0300

    Autoconnect (27.06 19:17)

--- Source/System/Backyard/Backyard.cpp
+++ Source/System/Backyard/Backyard.cpp
@@ -5,27 +5,45 @@
 namespace BackyardLocal
 {
 	static constexpr int32 UpdatePeriodMilliseconds = 100;
+	constexpr uint16 GameServerPort = 27020;
 	thread BackyardThread;
 }
+
 
 SBackyard::SBackyard(SNetwork& Network)
 	: Network(Network)
 {
 	// GENERATED CODE BEGIN
 	FRPCUtility::RegisterSystem(this, Hash("SBackyard"));
-	FRPCUtility::RegisterFunction(Hash("SBackyard"), Hash("BackyardRequestTest"), [](void* ThisPointer, const vector<byte>& Bytes, const FNetworkAddress& SourceAddress)
+	FRPCUtility::RegisterFunction(Hash("SBackyard"), Hash("BackyardRequestPlay"), [](void* ThisPointer, const vector<byte>& Bytes, const FNetworkAddress& SourceAddress)
 		{ 
 			const byte * Pointer = Bytes.data(); 
 			(void)Pointer;
 			(void)SourceAddress;
-			static_cast<SBackyard*>(ThisPointer)->BackyardRequestTest(SourceAddress);
+			static_cast<SBackyard*>(ThisPointer)->BackyardRequestPlay(SourceAddress);
 		});
-	FRPCUtility::RegisterFunction(Hash("SBackyard"), Hash("BackyardResponseTest"), [](void* ThisPointer, const vector<byte>& Bytes, const FNetworkAddress& SourceAddress)
+	FRPCUtility::RegisterFunction(Hash("SBackyard"), Hash("BackyardRequestBecameGameServer"), [](void* ThisPointer, const vector<byte>& Bytes, const FNetworkAddress& SourceAddress)
 		{ 
 			const byte * Pointer = Bytes.data(); 
 			(void)Pointer;
 			(void)SourceAddress;
-			static_cast<SBackyard*>(ThisPointer)->BackyardResponseTest(SourceAddress);
+			static_cast<SBackyard*>(ThisPointer)->BackyardRequestBecameGameServer(SourceAddress);
+		});
+	FRPCUtility::RegisterFunction(Hash("SBackyard"), Hash("BackyardResponseBecomeGameServer"), [](void* ThisPointer, const vector<byte>& Bytes, const FNetworkAddress& SourceAddress)
+		{ 
+			const byte * Pointer = Bytes.data(); 
+			(void)Pointer;
+			(void)SourceAddress;
+			static_cast<SBackyard*>(ThisPointer)->BackyardResponseBecomeGameServer(SourceAddress);
+		});
+	FRPCUtility::RegisterFunction(Hash("SBackyard"), Hash("BackyardResponseBecomeGameClient"), [](void* ThisPointer, const vector<byte>& Bytes, const FNetworkAddress& SourceAddress)
+		{ 
+			const byte * Pointer = Bytes.data(); 
+			(void)Pointer;
+			(void)SourceAddress;
+			std::remove_const<std::remove_reference<const FNetworkAddress &>::type>::type GameServerAddress;
+			FSerializationUtility::Deserialize(Pointer, GameServerAddress);
+			static_cast<SBackyard*>(ThisPointer)->BackyardResponseBecomeGameClient(SourceAddress, GameServerAddress);
 		});
 	// GENERATED CODE END
 }
@@ -42,8 +60,6 @@ void SBackyard::Initialize()
 			FPlayerState& Player = Players[Players.Create()];
 			Player.State = EPlayerState::None;
 			Player.Address = Address;
-
-			BackyardResponseTest(Address);
 		});
 
 
@@ -82,9 +98,9 @@ void SBackyard::Deinitialize()
 	BackyardLocal::BackyardThread.join();
 }
 
-void SBackyard::BackyardRequestTest(const FNetworkAddress& Address)
+void SBackyard::BackyardRequestPlay(const FNetworkAddress& Address)
 {
-	RPC_BODY(Network.BackyardRequest(Hash("BackyardRequestTest"), Address));
+	RPC_BODY(Network.BackyardRequest(Hash("BackyardRequestPlay"), Address));
 	const HPlayerState PlayerHandle = Players.Find(
 		[&Address](const FPlayerState& Player)
 		{
@@ -93,15 +109,65 @@ void SBackyard::BackyardRequestTest(const FNetworkAddress& Address)
 
 	if (PlayerHandle)
 	{
-		//FPlayerState& Player = Players[PlayerHandle];
-		Debugf("Received a backyard RPC from a registered player");
-		BackyardResponseTest(Address);
+		FPlayerState& Player = Players[PlayerHandle];
+		Debugf("Player %s asked to play", FStringUtility::ToString(Address).c_str());
+
+		const HPlayerState ServerPlayerHandle = Players.Find(
+			[](const FPlayerState& Player)
+			{
+				return Player.State == EPlayerState::GameServer;
+			});
+
+		if (ServerPlayerHandle)
+		{
+			FPlayerState& GameServerPlayer = Players[ServerPlayerHandle];
+			GameServerPlayer.ServerState.PendingClients.emplace_back(FPendingClient{ FTimeUtility::GetTime(), PlayerHandle });
+			Player.State = EPlayerState::BecomingGameClient;
+			FNetworkAddress GameServerAddress = GameServerPlayer.Address;
+			GameServerAddress.Port = BackyardLocal::GameServerPort;
+			BackyardResponseBecomeGameClient(Address, GameServerAddress);
+		} else
+		{
+			Player.State = EPlayerState::BecomingGameServer;
+			BackyardResponseBecomeGameServer(Address);
+		}
 	}
 }
 
-void SBackyard::BackyardResponseTest(const FNetworkAddress& Address)
+void SBackyard::BackyardRequestBecameGameServer(const FNetworkAddress& Address)
 {
-	RPC_BODY(Network.BackyardResponse(Hash("BackyardResponseTest"), Address));
+	RPC_BODY(Network.BackyardRequest(Hash("BackyardRequestBecameGameServer"), Address));
+	const HPlayerState PlayerHandle = Players.Find(
+		[&Address](const FPlayerState& Player)
+		{
+			return Player.Address == Address;
+		});
+
+	if (PlayerHandle)
+	{
+		FPlayerState& Player = Players[PlayerHandle];
+		if(Player.State == EPlayerState::BecomingGameServer)
+		{
+			Debugf("Player %s became game server", FStringUtility::ToString(Address).c_str());
+
+			Player.State = EPlayerState::GameServer;
+		} else
+		{
+			Debugf("Player %s became game server, but was not in correct state on backyard, ignoring", FStringUtility::ToString(Address).c_str());
+		}
+	}
+}
+
+void SBackyard::BackyardResponseBecomeGameServer(const FNetworkAddress& Address)
+{
+	RPC_BODY(Network.BackyardResponse(Hash("BackyardResponseBecomeGameServer"), Address));
+}
+
+void SBackyard::BackyardResponseBecomeGameClient(
+	const FNetworkAddress& Address,
+	const FNetworkAddress& GameServerAddress)
+{
+	RPC_BODY(Network.BackyardResponse(Hash("BackyardResponseBecomeGameClient"), Address, GameServerAddress));
 }
 
 void SBackyard::Update()
--- Source/System/Backyard/Backyard.h
+++ Source/System/Backyard/Backyard.h
@@ -3,6 +3,78 @@
 #include "BackyardTypes.h"
 #include "System/Network/Network.h"
 
+/*
+ * SM Draft
+ * HPlayerState PlayerHandle = Players.Find([&Message](const FPlayerState& InState)
+				{
+					return InState.Address == Message.Address;
+				});
+			if (PlayerHandle)
+			{
+				FPlayerState& Player = Players[PlayerHandle];
+
+				string String;
+				String.insert(0, reinterpret_cast<const char* const>(Message.Bytes.data()), Message.Bytes.size());
+
+				Debugf("String interpretation: %s", String.c_str());
+
+				if (String == "Play")
+				{
+					Debugf("Player %s asks to Play", NetworkUtility::ToString(Message.Address).c_str());
+
+					HPlayerState ServerPlayerStateHandle = Players.Find([](FPlayerState& InPlayer)
+						{
+							return InPlayer.State == EPlayerState::GameServer;
+						});
+					if (ServerPlayerStateHandle)
+					{
+
+						LowLevel.ServerSendMessage(ServerHandle, Player.Address, );
+					}
+
+					// TODO: if state was None, try to find server and send its IP, set state to BecomingGameClient, add PendingClient to state so others do not try connecting
+					// TODO: if no server found send message "You are the server", set state to BecomingGameServer
+				}
+				else if (String == "Server")
+				{
+					// TODO: if state was BecomingGameServer, add him to server list, set state to GameServer
+				}
+				else if (String == "CouldNotBecomeServer")
+				{
+					// TODO: if state was BecomingGameServer, temporarily blacklist server option from him, set state to None, do the Play
+				}
+				else if (String == "FailedToConnect")
+				{
+					// TODO: if state was BecomingGameClient, temporarily blacklist the server for the client, reset client state to None, do the Play for the client
+				}
+				else if (String == "Client")
+				{
+					// TODO: if state was BecomingGameClient, set state to GameClient
+				}
+				else if (String == "ClientConnected")
+				{
+					// TODO: if state was GameServer, add client to ConnectedClients
+				}
+				else if (String == "GameStarted") // TODO: server also lists all participants
+				{
+					// TODO: if state was GameServer, set state to GameServerStarted, remove server from server list
+				}
+				else if (String == "GameOver")
+				{
+					// TODO: if state was GameServerStarted, reset state to None for all participants
+				}
+				else if (String == "Disconnect")
+				{
+					// TODO: if state was GameServerStarted, reset state to None
+					// TODO: if state was GameClient, remove it from server ConnectedClients, reset state to None
+				}
+			}
+			else
+			{
+				Debugf("Unexpected message (PlayerState not found) dropped from %s", NetworkUtility::ToString(Message.Address).c_str());
+			}
+ */
+
 class SBackyard
 {
 public:
@@ -11,8 +83,10 @@ public:
 	void Initialize();
 	void Deinitialize();
 
-	void BackyardRequestTest(const FNetworkAddress& Address);
-	void BackyardResponseTest(const FNetworkAddress& Address);
+	void BackyardRequestPlay(const FNetworkAddress& Address);
+	void BackyardRequestBecameGameServer(const FNetworkAddress& Address);
+	void BackyardResponseBecomeGameServer(const FNetworkAddress& Address);
+	void BackyardResponseBecomeGameClient(const FNetworkAddress& Address, const FNetworkAddress& GameServerAddress);
 
 private:
 	SNetwork& Network;
